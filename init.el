d9aeb9eba927a6e609398ea3f92fc3c88cfda529
ilookers <ilookers@outlook.com>
2019-05-07 19:54:46 +0800

    emacs universal config

---
+++ init.el
@@ -0,0 +1,29 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+
+(require 'init-exec-path)
+
+(require 'init-ui)
+
+(require 'init-editing)
+
+(require 'init-navigation)
+
+(require 'init-miscellaneous)
+
+(require 'init-company-mode)
+
+(provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (exec-path-from-shell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/init-python.el
@@ -0,0 +1,6 @@
+(require 'init-elpa)
+
+(require 'elpy)
+(elpy-enable)
+
+(provide 'init-python)
